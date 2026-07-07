e016a673849a315561c625fa8898ae1b940e0f2a
HoshizoraArchive <akaibuhoshizora@gmail.com>
2026-07-08 02:06:05 +0900

    feat: load Chia notification copy enhancer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./authSessionDisplay.js";
 import "./buildVersionWatcher.js";
+import "./chiaNotificationExperience.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
